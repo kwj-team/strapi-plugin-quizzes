7e8ef2184f34d6bb4cde2918de1ace5034db6e4a
Wojciech Jaszczak <jaszczakw@gmail.com>
2022-06-03 14:18:21 +0200

    fix: Better reload

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,36 @@
 'use strict';
 
-module.exports = ({ strapi }) => {
+const { ApplicationError } = require('@strapi/utils').errors;
+const { getComponents } = require("./components");
+
+module.exports = async ({ strapi }) => {
   // bootstrap phase
+  let newlyCreated = 0;
+
+  const promises = getComponents().map(async ({ component, components }) => {
+    try {
+      await strapi.plugin('content-type-builder').services.components.createComponent({
+        component,
+        components
+      });
+      newlyCreated++;
+    } catch (e) {
+      if (e instanceof ApplicationError) {
+        if (e.message === "component.alreadyExists") {
+          return
+        }
+      }
+
+      throw e
+    }
+  });
+
+  await Promise.all(promises)
+
+  if (newlyCreated > 0) {
+    strapi.reload.isReloading = true
+    strapi.reload();
+    // TODO: More reliable way to wait for reload
+    await new Promise((resolve) => setTimeout(() => resolve(), 100));
+  }
 };
--- server/components/index.js
+++ server/components/index.js
@@ -1,17 +1,29 @@
 
+const { nameToSlug } = require("@strapi/utils")
+
 const answer = require('./kwj-components/answer.json')
 const question = require('./kwj-components/question.json')
 const singleChoice = require('./kwj-components/single-choice-question.json')
 const multiChoice = require('./kwj-components/multi-choice-question.json')
 
+const category = 'kwj-components'
+
 module.exports = {
     getComponents: () => {
-        return mapComponentToFormat([answer, question, multiChoice, singleChoice], 'kwj-components')
+        return [{
+            component: mapMainComponent(singleChoice), components: mapChildComponents([answer, question]),
+        }, {
+            component: mapMainComponent(multiChoice), components: mapChildComponents([answer, question]),
+        }]
     }
 }
 
-function mapComponentToFormat(components, category) {
+function mapChildComponents(components) {
     return components.map((component) => {
-        return { component: { ...component, ...component.info, category } }
+        return { ...mapMainComponent(component), tmpUID: `${nameToSlug(category)}.${nameToSlug(component.info.displayName)}` }
     })
+}
+
+function mapMainComponent(component) {
+    return { ...component, ...component.info, category }
 }
